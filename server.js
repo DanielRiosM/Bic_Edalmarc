1a0e895f1c7b8c9b9d0571fc937bdfdba4b99010
DanielRiosM <56373511+DanielRiosM@users.noreply.github.com>
2023-07-05 18:59:09 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+//Import env
+require("dotenv").config();
+//mongodb
+require("./config/db");
+
+const app = require("express")();
+
+//cors
+const cors = require("cors");
+app.use(cors());
+//For accepting post from data
+const bodyParser = require("express").json;
+app.use(
+  bodyParser({
+    limits: { fileSize: 3 * 1024 * 1024 },
+  })
+);
+//
+// const UserRouter = require("./api/User");
+//const AdminRouter = require("./api/administrator");
+//const MasonsRouter = require("./api/masons");
+// app.use("/user", UserRouter);
+//app.use("/administrator", AdminRouter);
+//app.use("/masons", MasonsRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`server running on port ${process.env.PORT}`);
+});
