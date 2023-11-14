39bf3cf0d61df3c028a8a042d21d8edba27ec013
Jesus Leyva <jesusleyva066@gmail.com>
2023-11-14 09:12:12 -0700

    acentos completados

--- api/client.js
+++ api/client.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const client = require("../models/client");
 
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 //Creacion del cliente
 router.post("/crearCliente", (req, res) => {
   let nombre = req.body.nombre;
